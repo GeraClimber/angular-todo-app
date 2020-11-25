03d9d8916b83356e998a54eb54a7c388628f55c8
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2020-11-25 12:13:25 +0300

    add styles and data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  date?: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   appTitle = 'angular-todo-app';
+  public todos: Todo[] = [{id: 1, title: 'Buy a beer', completed: false, date: new Date()},
+                          {id: 2, title: 'Buy a bread', completed: true, date: new Date()},
+                          {id: 3, title: 'Buy a milk', completed: true, date: new Date()}];
 }
